2ab4440bd1ae35c981894086ef210ca571b3a2a2
sathyam <panda.sathya9@gmail.com>
2017-04-23 23:32:42 +0530

    Header Compression

---
+++ srl_write/.net_dev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sathyam/Workspace/PE/srl_write/net_dev.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/sathyam/Workspace/PE/srl_write/net_dev.ko /home/sathyam/Workspace/PE/srl_write/net_dev.o /home/sathyam/Workspace/PE/srl_write/net_dev.mod.o ;  true
