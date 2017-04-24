94a845e5aa530b0db242c52523b671dc16ba0c37
niwinanto <niwinantop@gmail.com>
2017-04-24 10:27:38 +0530

    Delete .net_dev.ko.cmd

--- srl_write/.net_dev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sathyam/Workspace/PE/srl_write/net_dev.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/sathyam/Workspace/PE/srl_write/net_dev.ko /home/sathyam/Workspace/PE/srl_write/net_dev.o /home/sathyam/Workspace/PE/srl_write/net_dev.mod.o ;  true
