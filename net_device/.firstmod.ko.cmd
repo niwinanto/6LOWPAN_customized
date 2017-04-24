5df4d0574b3ae6fc5806c9ea81cf79d31f5d9693
niwinanto <niwinantop@gmail.com>
2017-04-24 10:03:39 +0530

    Delete .firstmod.ko.cmd

--- net_device/.firstmod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sathyam/Workspace/PE/net_device/firstmod.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/sathyam/Workspace/PE/net_device/firstmod.ko /home/sathyam/Workspace/PE/net_device/firstmod.o /home/sathyam/Workspace/PE/net_device/firstmod.mod.o ;  true
